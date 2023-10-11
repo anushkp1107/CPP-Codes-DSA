a0660feb8f927fcfd90b6348fb951fa1fd53f334
Romit  Dey <romit77dey@gmail.com>
2023-10-11 21:01:25 +0530

    added leetocode solutions under random LC folder

---
+++ Random LC/firstandlast.cpp
@@ -0,0 +1,78 @@
+// leetcode 34. Find First and Last Position of Element in Sorted Array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
+        int lo = findFirstOccurrence(nums, target);
+        int hi = findLastOccurrence(nums, target);
+        return {lo, hi};
+    }
+
+private:
+    int findFirstOccurrence(vector<int> &nums, int target)
+    {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        int ans = -1;
+
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] == target)
+            {
+                ans = mid;
+                hi = mid - 1;
+            }
+            else if (nums[mid] > target)
+            {
+                hi = mid - 1;
+            }
+            else
+            {
+                lo = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    int findLastOccurrence(vector<int> &nums, int target)
+    {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        int ans = -1;
+
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] == target)
+            {
+                ans = mid;
+                lo = mid + 1;
+            }
+            else if (nums[mid] > target)
+            {
+                hi = mid - 1;
+            }
+            else
+            {
+                lo = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
